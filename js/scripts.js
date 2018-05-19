f764a177e7d92f87ff21bcacc96ba2bf5d3b1f15
dromomaniac <evanlhendrix@gmail.com>
2018-05-19 15:32:46 +0200

    Validation works, can fill out form, add them as a row to table, and form resets

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,36 @@
 'use-strict';
-console.log(('hello world'));
-
 
 function addData() {
   // e.preventDefault();
 
-    var table = document.getElementById("table-one");
-    var row = table.insertRow(table.length);
-    console.log('table', table);
+    var tableOne = document.getElementById("table-one");
+    var formOne = document.getElementById("form-one");
+    var row = tableOne.insertRow(tableOne.length);
+    console.log('tableOne', tableOne);
     console.log('row: ', row);
+
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
     var cell5 = row.insertCell(4);
     var cell6 = row.insertCell(5);
-    cell1.innerHTML = "NEW CELL1";
-    cell2.innerHTML = "NEW CELL2";
-    cell3.innerHTML = "NEW CELL3";
-    cell4.innerHTML = "NEW CELL4";
-    cell5.innerHTML = "NEW CELL5";
-    cell6.innerHTML = "NEW CELL6";
+    cell1.innerHTML = document.querySelectorAll('#device-name')[0].value;
+    cell2.innerHTML = document.querySelectorAll('#device-type')[0].value;
+    cell3.innerHTML = document.querySelectorAll('#device-brand')[0].value;
+    cell4.innerHTML = document.querySelectorAll('#device-model')[0].value;
+    cell5.innerHTML = document.querySelectorAll('#device-install-date')[0].value;
+    cell6.innerHTML = 'trashcan';
+    // reset fields
+    formOne.reset();
 
 }
 
 
+
 // to be called on every form change. enables submit button when form is complete and disables it when not complete
 const validate = () => {
-  const form = document.getElementById('form');
+  const form = document.getElementById('form-one');
   console.log('form = ', form );
   console.log('form.elements = ', form.elements );
   const formElems = form.querySelectorAll('div > input');
