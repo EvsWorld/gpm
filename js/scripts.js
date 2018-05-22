524bdf714b096e82a3bf3f5eca224f855913e26e
dromomaniac <evanlhendrix@gmail.com>
2018-05-22 12:02:35 +0200

    refacter to combine fill row with render row

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,25 @@
 'use-strict';
+const tableOne = document.getElementById("table-one");
+const sortElement = document.querySelectorAll('#filter')[0];
+sortElement.onchange = renderTable.bind(null, store, tableOne);
 
 const store = [
-  // {
-  // deviceName: 'test name',
-  // deviceType: 'test Device',
-  // deviceBrand: 'test Brand',
-  // deviceModel: 'test model',
-  // deviceInstallDate: 'test install date',
-  // storeId: 'test1'
-  // }
+  {
+  deviceName: 'test name',
+  deviceType: 'test Device',
+  deviceBrand: 'test Brand',
+  deviceModel: 'test model',
+  deviceInstallDate: 300,
+  storeId: '28394576'
+  },
+  {
+  deviceName: 'duck man',
+  deviceType: 'phonebooth',
+  deviceBrand: 'Bell South',
+  deviceModel: 'T1000',
+  deviceInstallDate: 1988,
+  storeId: '28394576'
+  }
 ];
 
 function addData () {
@@ -27,34 +38,35 @@ function addData () {
   console.log('store = ', store);
   // render table
   // renderTable(store, tableOne);
-  renderLastRow(store, tableOne)
+  const post = Object.values(store[store.length-1]);
+  const tableRbody = tableOne.getElementsByTagName('tbody')[0];
+  renderRow(store, tableRbody, post)
   // reset fields
   formOne.reset();
 };
 
-const fillRow = (storeR, tableR, postData, row) => {
-  const id = postData.pop();
-  const indexToTackTrash = postData.length;
-  // cut off id from end (bc doesn't need to be rendered)
-  postData.forEach( (field, index) => {
-    let inserted = row.insertCell(index);
-    inserted.innerHTML = field;
-  });
-  let trashButton = row.insertCell(indexToTackTrash);
-  trashButton.innerHTML = `<i class='fas fa-trash'></i>`;
-  trashButton.onclick = deleteTheRow.bind(null, id, storeR, tableR)
+function renderRow (storeR, tableR, postToRender) {
+  console.trace('renderLastRow called!!!!')
+  const fillRow = (postData, row) => {
+    console.log('fillRow called!!');
+    const id = postData.pop();
+    const indexToTackTrash = postData.length;
+    // cut off id from end (bc doesn't need to be rendered)
+    postData.forEach( (field, index) => {
+      let inserted = row.insertCell(index);
+      inserted.innerHTML = field;
+    });
+    let trashButton = row.insertCell(indexToTackTrash);
+    trashButton.innerHTML = `<i class='fas fa-trash'></i>`;
+    trashButton.onclick = deleteTheRow.bind(null, id, storeR, tableR)
+  };
+  let newRow = tableR.insertRow(tableR.rows.length);
+  fillRow(postToRender, newRow);
 };
 
-
-function renderLastRow (storeR, tableR) {
-  const postToRender = Object.values(storeR[storeR.length-1]);
-  const tableRbody = tableR.getElementsByTagName('tbody')[0];
-  let newRow = tableRbody.insertRow(tableRbody.rows.length);
-  fillRow(storeR, tableRbody, postToRender, newRow);
-};
 
 function deleteTheRow (postId, storeR, tableR) {
-  console.log('DeleteTheRow called!!!');
+  console.trace('DeleteTheRow called!!!');
     storeR.forEach( (p,i) => {
       if (p.id === postId) {
         console.trace(`after enter 'if', storeR = `, storeR);
@@ -62,33 +74,39 @@ function deleteTheRow (postId, storeR, tableR) {
         console.trace('store after splice in deleteRow = ', storeR);
       }
     });
+    console.log('this = ', this);
     // now remove the elements and rerender the whole table
     renderTable(storeR, tableR)
   };
 
 // not using
 function renderTable (storeR, tableR) {
-  sortDevices(storeR, tableR)
-  console.log('enter renderTable!!!!!');
-  // clear table before rerender
-  // const tableRbody = tableR.getElementsByTagName('tbody')[0];
+  console.trace('enter renderTable!!!!!');
+  console.trace(`tableR = `, tableR);
+  const tableRbody = tableR.getElementsByTagName('tbody')[0];
+  console.trace(`tableRbody = `, tableRbody);
+  //still fucks up when this isnt here
+  // sortDevices(store, tableR)
+
   const new_tbody = document.createElement('tbody');
-  // render all store data to 'new_tbody'
-  storeR.forEach( post => {
-    let newRow = new_tbody.insertRow(tableR.length);
+  storeR.forEach( (post, i) => {
+    // for each object in store, create a new row in 'new_tbody'
     let postArray = Object.values(post);
-    fillRow(storeR, new_tbody, postArray, newRow);
+    let newRow = new_tbody.insertRow(i);
+    renderRow(storeR, tableR, postArray);
   });
   // replace old table body with 'new_tbody'
-  tableR.parentNode.replaceChild(new_tbody, tableR);
-  console.trace(`after renderTable, store = `, store);
+  // console.trace('tableR.parentNode = ', tableR.parentNode)
+  console.trace('FROM RENDERTABLE:  tableRbody = ', tableRbody)
+  tableR.parentNode.replaceChild(new_tbody, tableRbody);
+  console.trace(`end of renderTable, store = `, store);
 };
 
-function sortDevices () {
+function sortDevices (storeR) {
   const e = document.getElementById('filter');
   const sortBy = e.options[e.selectedIndex].value;
   console.log('sortBy = ', sortBy)
-  store.sort(function(a,b) {return (a[sortBy] > b[sortBy]) ? 1 : ((b[sortBy] > a[sortBy]) ? -1 : 0);} );
+  storeR.sort(function(a,b) {return (a[sortBy] > b[sortBy]) ? 1 : ((b[sortBy] > a[sortBy]) ? -1 : 0);} );
 }
 
 // to be called on every form change. enables submit button when form is complete and disables it when not complete
@@ -111,10 +129,3 @@ const validate = () => {
   let addButton = document.querySelectorAll('#add-button')[0];
   addButton.disabled = formIncomplete;
 };
-
-const sortElement = document.querySelectorAll('#filter')[0];
-const tableOne = document.getElementById("table-one");
-// inputElement.type = "button"
-sortElement.addEventListener('change', () => {
-    renderTable(store, tableOne);
-});
